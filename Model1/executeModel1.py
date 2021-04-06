4c8c1e2fef8173e64f8607bf6e28547c90c9d131
IrisBICS <iris.kremer.001@student.uni.lu>
2021-04-06 21:14:14 +0200

    Model 1 - version 2

--- Model1/executeModel1.py
+++ Model1/executeModel1.py
@@ -3,6 +3,8 @@ from model1 import Model1
 SAVEPATH = "../SavedModels/Model1/"
 DATAPATH = "../FER2013/icml_face_data.csv"
 
-model = Model1(SAVEPATH)
-#model.train(DATAPATH)
+model = Model1(SAVEPATH, DATAPATH)
+#model.train()
 model.loadModel()
+model.evaluateOnValid()
+model.evaluateOnTest()
--- Model1/model1.py
+++ Model1/model1.py
@@ -2,20 +2,27 @@ import os
 import csv
 import pandas as pd
 import numpy as np
+#import matplotlib.pyplot as plt
 from tensorflow.keras.models import Model, model_from_json
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.layers import *
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ReduceLROnPlateau
 from tensorflow.keras.utils import to_categorical
 
 
 class Model1:
 
-    def __init__(self, save_path, name="model1", verbose=True):
+    def __init__(self, save_path, data_path, name="model1", verbose=True):
+
+        if verbose:
+            print("\nCreating model object...")
+
         self.save_path = save_path
         self.name = name
         self.verbose = verbose
+
+        self.__loadData(data_path)
 
         if self.verbose:
             print("\nModel object created successfully!")
@@ -49,7 +56,7 @@ class Model1:
         # Extracting data from csv
         emotion_data = pd.read_csv(path, sep=r'\s*,\s*', engine='python')
         emotion_data.drop(columns=['Usage'])  # I am partitioning the data usage myself
-        emotion_data = emotion_data.sample(frac=1).reset_index(drop=True)  # Shuffling data
+        emotion_data = emotion_data.sample(frac=1, random_state=0).reset_index(drop=True)  # Shuffling data, seeded
 
         all_data = []
         all_labels = []
@@ -63,7 +70,7 @@ class Model1:
         all_data = np.array(all_data)
         all_labels = np.array(all_labels)
 
-        all_data = all_data.reshape(all_data.shape[0], 48, 48)
+        all_data = all_data.reshape((all_data.shape[0], 48, 48))
         all_data = np.stack((all_data, all_data, all_data), axis=-1)
 
         all_labels = to_categorical(all_labels, num_classes=7)
@@ -87,25 +94,29 @@ class Model1:
             print("Extracted", len(self.x_testing_data), "data entries for testing.")
             print("Finished loading data.")
 
+        #plt.imshow(self.x_training_data[10000])  # Plot one of the face images to see it
+        #plt.show()
+
     def __prepareModel(self, epochs, batch_size):
 
         self.epochs = epochs
         self.batch_size = batch_size
 
         self.callbacks = []
-        self.callbacks.append(ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, mode='auto'))
+        self.callbacks.append(ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, verbose=1, mode='auto'))
 
-    def train(self, data_path, epochs=25, batch_size=32):
+    def train(self, epochs=10, batch_size=32, learning_rate=0.0001):
 
-        self.__loadData(data_path)
         self.__createArchitecture()
         self.__prepareModel(epochs, batch_size)
 
         if self.verbose:
             print("\nStarting training...")
 
-        self.model.compile(loss="categorical_crossentropy", optimizer=SGD(learning_rate=0.0005), metrics=["accuracy"])
-        self.history = self.model.fit(self.x_training_data, self.y_training_data, batch_size=32, epochs=30, verbose=1, validation_data=(self.x_validation_data, self.y_validation_data), callbacks=self.callbacks)
+        self.optimizer = Adam(learning_rate=learning_rate)
+        self.model.compile(loss="categorical_crossentropy", optimizer=self.optimizer, metrics=["accuracy"])
+
+        self.history = self.model.fit(self.x_training_data, self.y_training_data, batch_size=self.batch_size, epochs=self.epochs, verbose=1, validation_data=(self.x_validation_data, self.y_validation_data), callbacks=self.callbacks)
 
         if self.verbose:
             print("Finished training.")
@@ -152,5 +163,41 @@ class Model1:
                     historyCols = [[value] for value in row]
         self.history = {c[0]: c[1:] for c in historyCols}
 
+        self.optimizer = Adam(learning_rate=self.history["lr"][-1])  # Take last lr value as lr
+        self.model.compile(loss="categorical_crossentropy", optimizer=self.optimizer, metrics=["accuracy"])
+
         if self.verbose:
             print("Finished loading model.")
+
+    def __evaluate(self, x, y):
+
+        loss_and_metrics = self.model.evaluate(x, y)
+        print("Loss: ", loss_and_metrics[0], "\nAccuracy: ", loss_and_metrics[1])
+
+    def evaluateOnValid(self):
+
+        if not self.model:
+            print("\nError: Please load or train the model first!")
+            return
+
+        if self.verbose:
+            print("\nEvaluating the model on the validation data...")
+
+        self.__evaluate(self.x_validation_data, self.y_validation_data)
+
+        if self.verbose:
+            print("Finished evaluating the model on the validation data.")
+
+    def evaluateOnTest(self):
+
+        if not self.model:
+            print("\nError: Please load or train the model first!")
+            return
+
+        if self.verbose:
+            print("\nEvaluating the model on the unseen testing data...")
+
+        self.__evaluate(self.x_testing_data, self.y_testing_data)
+
+        if self.verbose:
+            print("Finished evaluating the model on the unseen testing data.")
